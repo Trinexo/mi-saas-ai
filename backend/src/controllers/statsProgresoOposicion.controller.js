168c7a4f9dfd96027b0fd899371167708d03c63c
Trinexo <informatica@trinexo.com>
2026-04-07 14:47:55 +0200

    refactor: split statsProgreso.controller.js into Tema + Oposicion (#213)

---
+++ backend/src/controllers/statsProgresoOposicion.controller.js
@@ -0,0 +1,54 @@
+import { ok } from '../utils/response.js';
+import { statsService } from '../services/stats.service.js';
+import { ApiError } from '../utils/api-error.js';
+
+export const getSimulacrosStats = async (req, res, next) => {
+  try {
+    const data = await statsService.getSimulacrosStats(req.user.userId, req.query.oposicion_id);
+    return ok(res, data);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getResumenOposicion = async (req, res, next) => {
+  try {
+    const oposicionId = req.query.oposicion_id ? Number(req.query.oposicion_id) : null;
+    if (!oposicionId) return next(new ApiError(400, 'Se requiere oposicion_id'));
+    const data = await statsService.getResumenOposicion(req.user.userId, oposicionId);
+    return ok(res, data);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getProgresoMaterias = async (req, res, next) => {
+  try {
+    const oposicionId = req.query.oposicion_id ? Number(req.query.oposicion_id) : null;
+    if (!oposicionId) return next(new ApiError(400, 'Se requiere oposicion_id'));
+    const data = await statsService.getProgresoMaterias(req.user.userId, oposicionId);
+    return ok(res, data);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getProgresoTemasByMateria = async (req, res, next) => {
+  try {
+    const materiaId = req.query.materia_id ? Number(req.query.materia_id) : null;
+    if (!materiaId) return next(new ApiError(400, 'Se requiere materia_id'));
+    const data = await statsService.getProgresoTemasByMateria(req.user.userId, materiaId);
+    return ok(res, data);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getMisOposiciones = async (req, res, next) => {
+  try {
+    const data = await statsService.getMisOposiciones(req.user.userId);
+    return ok(res, data);
+  } catch (error) {
+    return next(error);
+  }
+};
